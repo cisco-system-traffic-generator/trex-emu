292c6a6a322cdacf9cc64956a5fd9ac041f6db58
Hanoh <hhaim@cisco.com>
2019-09-18 17:05:38 +0300

    RPC is a seperate files with a channel
    Signed-off-by: Hanoh <hhaim@cisco.com>

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"emu/rpc"
+	"encoding/hex"
+	"fmt"
 	"math/rand"
 	"time"
 )
 
+type pkt []byte
+
+func newPktSize(size uint16) pkt {
+	var mbuf pkt
+	mbuf = make(pkt, size)
+	return mbuf
+}
+
+func freePkt() {
+
+}
+
+func test1() {
+	size := 1
+	for i := 0; i <= size; i++ {
+		mbuf := newPktSize(128)
+		mbuf[0] = 7
+		fmt.Printf("%d-%s", len(mbuf), hex.Dump(mbuf))
+
+	}
+}
+
 func main() {
+	test1()
+	return
 	rand.Seed(time.Now().UnixNano())
 	rpc.RcpCtx.Create(4510)
 	rpc.RcpCtx.StartRxThread()
@@ -19,6 +45,5 @@ func main() {
 	}
 
 	rpc.RcpCtx.Delete()
-
 	return
 }
