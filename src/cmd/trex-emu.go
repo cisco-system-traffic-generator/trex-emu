40831895541a7c9d38cdf256523c4413d99a2430
Hanoch Haim <hhaim@cisco.com>
2020-03-15 11:50:40 +0200

    WIP
    Signed-off-by: Hanoch Haim <hhaim@cisco.com>

--- src/cmd/trex-emu.go
+++ src/cmd/trex-emu.go
@@ -21,6 +21,10 @@ import (
 	"emu/plugins/ipv6"
 )
 
+const (
+	VERSION = "0.1"
+)
+
 func RegisterPlugins(tctx *core.CThreadCtx) {
 	arp.Register(tctx)
 	icmp.Register(tctx)
@@ -31,39 +35,41 @@ func RegisterPlugins(tctx *core.CThreadCtx) {
 }
 
 type MainArgs struct {
-	port      int
-	verbose   bool
-	sim       bool
-	capture   bool
-	monitor   bool
-	time      time.Duration
-	file      string
-	dummyVeth bool
-	vethPort  int
-	zmqServer string
+	port      *int
+	verbose   *bool
+	sim       *bool
+	capture   *bool
+	monitor   *bool
+	time      *int
+	file      *string
+	dummyVeth *bool
+	vethPort  *int
+	zmqServer *string
+	version   *bool
+	duration  time.Duration
 }
 
 func parseMainArgs() *MainArgs {
 	var args MainArgs
 	parser := argparse.NewParser("Emu Server", "Emu server emulates clients and namespaces")
 
-	args.port = *parser.Int("p", "rpc port", &argparse.Options{Default: 4510, Help: "RPC Port for server"})
-	args.vethPort = *parser.Int("l", "veth zmq port", &argparse.Options{Default: 4511, Help: "Veth Port for server"})
-	args.verbose = *parser.Flag("v", "verbose", &argparse.Options{Default: false, Help: "Run server in verbose mode"})
-	args.sim = *parser.Flag("s", "simulator", &argparse.Options{Default: false, Help: "Run server in simulator mode"})
-	args.zmqServer = *parser.String("S", "zmq-server", &argparse.Options{Default: "127.0.0.1", Help: "zmq server ip"})
-	args.capture = *parser.Flag("c", "capture", &argparse.Options{Default: false, Help: "Run server in capture mode"})
-	args.monitor = *parser.Flag("m", "monitor", &argparse.Options{Default: false, Help: "Run server in K12 monitor mode"})
-	args.time = time.Duration(*parser.Int("t", "time", &argparse.Options{Default: 10, Help: "Time of the simulation in sec"}))
-	args.file = *parser.String("f", "file", &argparse.Options{Default: "emu_file", Help: "Path to save the pcap file"})
-	args.dummyVeth = *parser.Flag("d", "dummy-veth", &argparse.Options{Default: false, Help: "Run server with a dummy veth, all packets to rx will be dropped"})
+	args.port = parser.Int("p", "rpc port", &argparse.Options{Default: 4510, Help: "RPC Port for server"})
+	args.vethPort = parser.Int("l", "veth zmq port", &argparse.Options{Default: 4511, Help: "Veth Port for server"})
+	args.verbose = parser.Flag("v", "verbose", &argparse.Options{Default: false, Help: "Run server in verbose mode"})
+	args.sim = parser.Flag("s", "simulator", &argparse.Options{Default: false, Help: "Run server in simulator mode"})
+	args.zmqServer = parser.String("S", "zmq-server", &argparse.Options{Default: "127.0.0.1", Help: "zmq server ip"})
+	args.capture = parser.Flag("c", "capture", &argparse.Options{Default: false, Help: "Run server in capture mode"})
+	args.monitor = parser.Flag("m", "monitor", &argparse.Options{Default: false, Help: "Run server in K12 monitor mode"})
+	args.time = parser.Int("t", "time", &argparse.Options{Default: 10, Help: "Time of the simulation in sec"})
+	args.file = parser.String("f", "file", &argparse.Options{Default: "emu_file", Help: "Path to save the pcap file"})
+	args.dummyVeth = parser.Flag("d", "dummy-veth", &argparse.Options{Default: false, Help: "Run server with a dummy veth, all packets to rx will be dropped"})
+	args.version = parser.Flag("V", "version", &argparse.Options{Default: false, Help: "show trex-emu version"})
 
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
 	}
-
-	args.time = args.time * time.Second
+	args.duration = time.Duration(*args.time) * time.Second
 
 	return &args
 }
@@ -72,38 +78,43 @@ func RunCoreZmq(args *MainArgs) {
 
 	var zmqVeth core.VethIFZmq
 
-	port := uint16(args.port)
-	fmt.Printf("run zmq server on [%d:rx:%d:tx:%d] \n", port, args.vethPort, args.vethPort+1)
+	if *args.version {
+		fmt.Printf("tre-emu version is %s \n", VERSION)
+		os.Exit(0)
+	}
+
+	port := uint16(*args.port)
+	fmt.Printf("run zmq server on [%d:rx:%d:tx:%d] \n", port, *args.vethPort, *args.vethPort+1)
 	rand.Seed(time.Now().UnixNano())
 
 	var simrx core.VethIFSim
-	if args.dummyVeth {
+	if *args.dummyVeth {
 		var simVeth core.VethSink
 		simrx = &simVeth
 	}
 
-	tctx := core.NewThreadCtx(0, port, args.sim, &simrx)
+	tctx := core.NewThreadCtx(0, port, *args.sim, &simrx)
 
-	if !args.sim {
-		zmqVeth.Create(tctx, uint16(args.vethPort), args.zmqServer)
+	if !*args.sim {
+		zmqVeth.Create(tctx, uint16(*args.vethPort), *args.zmqServer)
 		zmqVeth.StartRxThread()
 		tctx.SetZmqVeth(&zmqVeth)
 	}
 
 	RegisterPlugins(tctx)
 
-	tctx.SetVerbose(args.verbose)
-	tctx.Veth.SetDebug(args.monitor, args.capture)
+	tctx.SetVerbose(*args.verbose)
+	tctx.Veth.SetDebug(*args.monitor, *args.capture)
 	tctx.StartRxThread()
 	defer tctx.Delete()
 
-	if !args.sim {
+	if !*args.sim {
 		tctx.MainLoop()
 	} else {
-		tctx.MainLoopSim(args.time)
+		tctx.MainLoopSim(args.duration)
 	}
-	if args.capture {
-		tctx.SimRecordExport(args.file)
+	if *args.capture {
+		tctx.SimRecordExport(*args.file)
 	}
 }
 
